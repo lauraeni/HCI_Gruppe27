28fecde06e6f555a533e0fa67ab40ffede4c116b
Verena Azzalini <verena.azzalini@uni-hannover.de>
2024-12-29 10:18:02 +0100

    Gruppenchat Funktion

---
+++ Groupchat/script.js
@@ -0,0 +1,28 @@
+/*
+Das Folgende ist dafür da, damit die Funktionen wie das senden einer Nachricht, das aktualisieren des Chats, 
+das Schließen des Fensters und das Scrollen des Fensters funktionieren. 
+TODO: Es muss noch implementiert werden, dass die Hompage wieder erscheint, wenn das Fenster geschlossen wird.
+Wichtig: Man kann Nachrichten senden, doch diese werden nicht abgespeichert. Bei erneuter Öffnung des Chats
+wird der Chatverlauf wieder auf dem ursprünglichem Stand sein.  
+*/
+document.querySelector('.send-button').addEventListener('click', function() {
+    const messageInput = document.querySelector('.input-field');
+    const messageText = messageInput.value;
+    
+    if (messageText) {
+        const chatWindow = document.querySelector('.chat-window');
+        const newMessage = document.createElement('div');
+        newMessage.classList.add('message');
+        newMessage.innerHTML = `
+            <div class="message-user">Verena:</div> 
+            <div class="message-text">${messageText}</div>
+        `;
+        chatWindow.appendChild(newMessage);
+        messageInput.value = ''; 
+        chatWindow.scrollTop = chatWindow.scrollHeight; 
+    }
+});
+document.getElementById('closeButton').addEventListener('click', function() {
+    const chatContainer = document.querySelector('.chat-container');
+    chatContainer.style.display = 'none'; 
+});
